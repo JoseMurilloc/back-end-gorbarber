fdafa11b2b833bdbde4c714fa03da1348690f0ad
Jose Murillo <jooseemurillo@gmail.com>
2020-11-03 13:07:42 -0300

    testing user authentication

--- src/modules/users/services/CreateSessionServices.spec.ts
+++ src/modules/users/services/CreateSessionServices.spec.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import AppError from '@shared/infra/http/errors/AppError';
 import CreateUserServices from '@modules/users/services/CreateUserServices';
 import CreateSessionServices from '@modules/users/services/CreateSessionServices';
 import FakeUserRepository from '../infra/typeorm/repositories/fakes/FakeUserRepository';
@@ -35,4 +36,54 @@ describe('AuthenticationUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+
+  it('should be able to authentication with non existing user', async () => {
+    const fakeUsersRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticationUser = new CreateSessionServices(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticationUser.execute({
+        email: 'john@gmail.com',
+        password: '123456'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+  });
+
+  it('should not be able to authenticated is wrong password', async () => {
+    const fakeUsersRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticationUser = new CreateSessionServices(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+
+    const createUser = new CreateUserServices(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      password: '123456'
+    })
+
+
+    expect(
+      authenticationUser.execute({
+        email: 'john@gmail.com',
+        password: 'wrong-123456'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
 });
